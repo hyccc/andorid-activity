06236948ec1de4d6f5a0c81e1b537261c848b613
technology orange <694481479@qq.com>
2016-11-23 18:36:05 +0800

    change
    i thought  some files are wrong

--- activity/src/com/example/activity/MainActivity.java
+++ activity/src/com/example/activity/MainActivity.java
@@ -2,33 +2,56 @@ package com.example.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private static final String tag = "MainActivity";
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public void onCreate(Bundle saveInstanceState) {
+		super.onCreate(saveInstanceState);
+		
+		Log.e(tag, "start onCreate!!");
 	}
+	
+	protected void onStart() {
+		super.onStart();
+		
+		Log.e(tag, "start onStart!!");
+	}
+	
+	protected void onRestart() {
+		super.onRestart();
+		
+		Log.e(tag, "start onRestart!!");
+	}	
+	
+	protected void onResume() {
+		super.onResume();
+		
+		Log.e(tag, "start onResume");
+		
+	}
+	
+	protected void onPause() {
+		super.onPause();
+		
+		Log.e(tag, "start onPause!!");
+	}
+	
+    protected void onStop() {
+    	super.onStop();
+    	
+    	Log.e(tag, "start onStop!!");
+	}
+    
+    protected void onDestroy() {
+    	super.onDestroy();
+    	
+    	Log.e(tag, "onDestroy!!");
+	}
+	
 }
